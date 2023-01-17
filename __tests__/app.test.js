28f92583c3619faa3c1ba2e8fee2a02896cd2bfb
Catherine Li <manheicatherine@gmail.com>
2023-01-17 14:07:08 +0000

    updated

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -29,12 +29,34 @@ describe("API Testing", () => {
   });
 
   describe.only("Ticket 4: GET /api/articles", () => {
+    // test("returns an array of articles which is sorted by date in descending order ", () => {
+    //   return request(app)
+    //     .get("/api/articles")
+    //     .expect(200)
+    //     .then(({ body }) => {
+    //       body.articles.forEach((articles) => {
+    //         expect(articles).toEqual(
+    //           expect.objectContaining({
+    //             article_id: expect.any(Number),
+    //             title: expect.any(String),
+    //             topic: expect.any(String),
+    //             author: expect.any(String),
+    //             created_at: expect.any(Number),
+    //             votes: expect.any(Number),
+    //             article_img_url: expect.any(String),
+    //             comment_count: expect.any(String),
+    //           })
+    //         );
+    //         expect(body.length).toBe(12);
+    //       });
+    //     });
+    // });
     test("Error handling", () => {
       return request(app)
         .get("/api/articles")
-        .expect(404)
-        .then(({ body }) => {
-          expect(body.msg).toBe("Path not found!");
+        .expect(204)
+        .then(({body}) => {
+            expect(body.msg).toBe('Path not found!')
         });
     });
   });
--- models/articles.models.js
+++ models/articles.models.js
@@ -1,23 +1,39 @@
 const db = require("../db/connection");
 
 exports.fetchArticles = () => {
-  return db
-    .query(
-      `SELECT * FROM articles LEFT OUTER JOIN comments ON comments.created_at = articles.created_at ORDER BY articles.created_at DESC;`
-    )
-    .then(({ rows }) => {
-      const addCommentCount = rows.map((eachObj) => {
-        delete eachObj.body;
-        delete eachObj.comment_id;
-        eachObj.comment_count = eachObj.article_id;
-        return eachObj;
+//   const commentArr = db.query(`SELECT * FROM comments;`).then(({ rows }) => {
+//     const commentObj = rows.map((each) => {
+//       return each.article_id;
+//     });
+
+//     const counts = {};
+//     const keyVal = commentObj.forEach((x) => {
+//       counts[x] = (counts[x] || 0) + 1;
+//     });
+
+    const articleArr = db
+      .query(`SELECT * FROM articles ORDER BY created_at DESC;`)
+      .then(({ rows }) => {
+        const deleteUnuseItem = rows.map((each) => {
+        //   each.comment_count = counts[(each.article_id)];
+          delete each.body;
+          return each;
+        });
+        console.log(deleteUnuseItem);
       });
-      const checkVal = Object.values(addCommentCount[0]);
-      
-      if (checkVal.includes(null)) {
-        return Promise.reject({ status: 404, msg: `Path not found!` });
-      } else {
-        return addCommentCount;
-      }
-    });
+//   });
 };
+
+//   const addCommentCount = rows.map((eachObj) => {
+//     delete eachObj.body;
+//     delete eachObj.comment_id;
+//     eachObj.comment_count = eachObj.article_id;
+//     return eachObj;
+//   });
+//   const checkVal = Object.values(addCommentCount[0]);
+
+//   if (checkVal.includes(null)) {
+//     return Promise.reject({ status: 404, msg: `Path not found!` });
+//   } else {
+//     return addCommentCount;
+//   }
