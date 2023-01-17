f18dc35b0d17369c0a87a2012ce890277dd3002a
Catherine Li <manheicatherine@gmail.com>
2023-01-17 16:44:34 +0000

    completed ticket 5

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -46,41 +46,37 @@ describe("API Testing", () => {
         .get("/api/articles/1")
         .expect(200)
         .then(({ body }) => {
-          const objOfArr = body.articles;
-          expect(objOfArr.length).toBe(1);
-
-          objOfArr.forEach((obj) => {
-            expect(obj).toEqual(
-              expect.objectContaining({
-                article_id: expect.any(Number),
-                title: expect.any(String),
-                topic: expect.any(String),
-                author: expect.any(String),
-                body: expect.any(String),
-                created_at: expect.any(String),
-                votes: expect.any(Number),
-                article_img_url: expect.any(String),
-              })
-            );
-          });
+          const article = body.article;
+          expect(article.length).toBe(1);
+          expect(article).toEqual([{
+            article_id: 1,
+            title: "Living in the shadow of a great man",
+            topic: "mitch",
+            author: "butter_bridge",
+            body: "I find this existence challenging",
+            created_at: "2020-07-09T20:11:00.000Z",
+            votes: 100,
+            article_img_url:
+              "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
+          }]);
         });
     });
-    test("Error handling 1: passing a valid but not existing id", () => {
-      return request(app)
-        .get("/api/articles/100")
-        .expect(404)
-        .then(({body}) => {
-          expect(body.msg).toBe("404 Not Found");
-        });
-    });
+  });
+  test("Error handling 1: passing a valid but not existing id", () => {
+    return request(app)
+      .get("/api/articles/100")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("404 Not Found");
+      });
+  });
 
-    test("Error handling 2: passing a invalid id", () => {
-        return request(app)
-          .get("/api/articles/hellokitty")
-          .expect(400)
-          .then(({body}) => {
-            expect(body.msg).toBe("Bad request");
-          });
+  test("Error handling 2: passing a invalid id", () => {
+    return request(app)
+      .get("/api/articles/hellokitty")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad request");
       });
   });
 });
--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -12,8 +12,8 @@ exports.getArticles = (req, res, next) => {
 exports.getArticlesById = (req, res, next) => {
   const { article_id } = req.params;
   fetchArticlesById(article_id)
-    .then((articles) => {
-      res.status(200).send({ articles });
+    .then((article) => {
+      res.status(200).send({ article });
     })
     .catch((error) => {
       next(error);
